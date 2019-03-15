20e10d4802d2bea20e98714b619fb66a0c274427
Msaezcardenas <msaezcardernas@gmail.com>
2019-03-14 21:26:31 -0300

    primer test hecho

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import NameForm from './Component/NameForm';
+
+import { shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+ const div = document.createElement('div');
+ ReactDOM.render(<App />, div);
+ ReactDOM.unmountComponentAtNode(div);
 });
 
+describe('debería encontrar el componente NameForm en App',()=>{
+ const find = shallow(<App />);
+ expect(find.find(NameForm).length).toBe(1);
+});
